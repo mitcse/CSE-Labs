43f962e3d484a63ce525b5565e517623810413de
ykl7 <yklal95@gmail.com>
2015-09-10 17:43:22 +0530

    shortened ds lab 5 infix to prefix code

--- DS Lab/Lab 05/InfixToPrefix.c
+++ DS Lab/Lab 05/InfixToPrefix.c
@@ -1,177 +1,111 @@
-//
-//  InfixToPrefix.c
-//  Convertnig Infix expression to Prefix
-//
-//  Created by Avikant Saini on 8/31/15.
-//  Copyright © 2015 avikantz. All rights reserved.
-//
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+#include<math.h>
 
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
+# define SIZE 1000
 
-#define SIZE 1000
-#define UNDERFLOW_CHAR '\0'
-
-// Boolean type, just for readability
-
-typedef enum {
-	NO = 0,
-	YES = 1,
-} BOOL;
-
-// Stack methods for character stack
-
-BOOL isStackFull (int tos) {
-	if (tos == SIZE - 1)
-		return YES;
-	return NO;
-}
-
-BOOL isStackEmpty (int tos) {
-	if (tos == -1)
-		return YES;
-	return NO;
-}
-
-void push (char *stack, char item, int *tos) {
-	if (*tos == SIZE - 1)
+void push (char *s, char e, int *top)
+{
+	if (*top == SIZE-1)
 		return;
-	(*tos) += 1;
-	*(stack + (*tos)) = item;
+	(*top) += 1;
+	*(s+(*top)) = e;
 }
 
-char pop (char *stack, int *tos) {
-	if (*tos == -1)
-		return UNDERFLOW_CHAR;
-	return *(stack + ((*tos)--));
+char pop (char *s, int *top)
+{
+	if (*top == -1)
+		return -32767;
+	return *(s+((*top)--));
 }
 
-// Reversing stack
-
-void reverse (char *stack, int tos) {
+void reverse (char *s, int top)
+{
 	int i;
-	for (i = 0; i <= tos/2; ++i) {
-		char ch = *(stack + i);
-		*(stack + i) = *(stack + tos - i);
-		*(stack + tos - i) = ch;
+	for (i=0; i<=top/2; i++)
+	{
+		char c = *(s + i);
+		*(s + i) = *(s + top - i);
+		*(s + top - i) = c;
 	}
 }
 
-// Get the index of a character in a string
-
-int indexOf (char character, char *string) {
-	char *ptr = strchr(string, character);
-	if (ptr)
-		return (int)(ptr - string);
+int indexOf (char  c, char *s)
+{
+	char *p = strchr(s, c);
+	if (p)
+		return (int) (p-s);
 	return -1;
 }
 
-// Checking of the character is an operator
-
-BOOL isOperator (char op) {
-	if (indexOf(op, "+-*/%$") != -1)
-		return YES;
-	return NO;
+int isOp (char op)
+{
+	if (indexOf(op, "+-*/") != -1)
+		return 1;
+	return 0;
 }
 
-// Checking if the character is an operand
-
-BOOL isOperand (char op) {
-	if ((op >= 65 && op <= 90) || (op >= 97 && op <= 122))
-		return YES;
-	if (op >= 48 && op <= 57)
-		return YES;
-	return NO;
+int prec (char op)
+{
+	if (indexOf(op, ")]}") != -1)
+		return 0;
+	else if (indexOf(op, "+-") != -1)
+		return 1;
+	else if (indexOf(op, "*/") != -1)
+		return 2;
+	else
+		return -1;
 }
 
-// Operator Precedence
-
-int operatorPrecedence (char op) {
-	if (indexOf(op, ")]}") != -1) return 0;
-	else if (indexOf(op, "+-") != -1) return 1;
-	else if (indexOf(op, "*/%") != -1) return 2;
-	else if (op == '$') return 3;
-	return -1;
-}
-
-/**
- *	The algorithm goes as follows.
- *
- *	Reverse the Expression and parse the inputs in the expression one by one.
- *
- *	1. If the input is an operand, then place it in the output buffer.
- *
- *  2. If the input is an operator, push it into the stack.
- *
- *	3. While the stack is not empty and operator in stack has higher precedence than input operator, then pop the operator present in stack and add it to output buffer.
- *
- *	4. Push the input operator into the output stack.
- *
- *	5. If the input is an close brace, push it into the stack.
- *
- *	6. If the input is a open brace, pop elements in stack one by one until we encounter open brace. Discard braces while writing to output buffer.
- *
- *	7. Display the output buffer in reverse order.
- */
-
-char * toPrefix (char * exp) {
-	
-	int tosp = -1;
-	char *prefix = (char *)calloc(SIZE, sizeof(char));
-	int toso = -1;
-	char *operator = (char *)calloc(SIZE, sizeof(char));
-	
-	int l = (int)strlen(exp);
-	int i;
-	
-	for (i = l - 1; i >= 0; --i) {
-		char z = *(exp + i);
-		
-		if (isOperand(z))
-			push(prefix, z, &tosp);
-		
-		else if (operatorPrecedence(z) == 0)
-			push(operator, z, &toso);
-		
-		else if (isOperator(z)) {
-			while (!isStackEmpty(toso) && operatorPrecedence(z) < operatorPrecedence(*(operator + toso))) {
-				char op =  pop(operator, &toso);
-				if (isOperator(op))
-					push(prefix, op, &tosp);
+char *toPrefixExpn (char *e)
+{
+	int top1, top2;
+	top1 = -1;
+	top2 = -1;
+	char *pre = (char *) calloc (SIZE, sizeof(char));
+	char *op = (char *) calloc (SIZE, sizeof(char));
+	int l, i;
+	l = strlen(e);
+	for (i=l-1; i>=0; --i)
+	{
+		char z = *(e+i);
+		if (isdigit(z) || isalpha(z))
+			push (pre, z, &top1);
+		else if (prec(z) == 0)
+			push (op, z, &top2);
+		else if (isOp(z))
+		{
+			while ((top2 != -1) && (prec(z) < prec(*(op+top2))))
+			{
+				char o = pop (op, &top1);
+				if (isOp(o))
+					push (pre, o, &top1);
 			}
-			push(operator, z, &toso);
+			push (op, z, &top2);
 		}
-		
-		else if (indexOf(z, "([{") != -1) {
-			while (operatorPrecedence(*(operator + toso)) != 0)
-				push(prefix, pop(operator, &toso), &tosp);
-			pop(operator, &toso);
+		else if (indexOf (z, "([{") != -1)
+		{
+			while (prec(*(op + top2)) != 0)
+				push (pre, pop (op, &top2), &top1);
+			pop (op, &top2);
 		}
-		
 		else
 			continue;
-		
 	}
-	
-	while(!isStackEmpty(toso))
-		push(prefix, pop(operator, &toso), &tosp);
-	
-	reverse(prefix, tosp);
-	return prefix;
+	while (top2 != -1)
+		push (pre, pop (op, &top2), &top1);
+	reverse (pre, top1);
+	return pre;
 }
 
-int main(int argc, const char * argv[]) {
-	
-	char *infix = (char *)calloc(SIZE, sizeof(char));
-	
-	printf("\n\tThis program will convert an Infix expression to Prefix.\n\te.g. (1 + (4 * 5)) = +*145.\n\n\tEnter an valid parenthesized Infix expression : ");
-	fgets(infix, SIZE, stdin);
-	
-	char *prefix = toPrefix(infix);
-	
-	printf("\n\n\tInfix: %s\n\tPrefix: %s\n\n", infix, prefix);
-	
+int main()
+{
+	char *expn = (char *) calloc (SIZE, sizeof(char));
+	printf ("Enter expression\n");
+	scanf ("%s", expn);
+	char *p = toPrefixExpn (expn);
+	printf ("Result is %s\n", p);
 	return 0;
 }
