83c030066bfbe5d546b9edda4486c1cc4be38ce7
Avikant Saini <avikantsainidbz@gmail.com>
2016-04-21 15:04:37 +0530

    DBS Lab 11 Update

--- DBMS Lab/Triggers/03.sql
+++ DBMS Lab/Triggers/03.sql
@@ -1,40 +1,50 @@
-DROP TABLE Account;
+DROP TABLE ZAccount;
 
-CREATE TABLE Account (
+CREATE TABLE ZAccount (
 	ano VARCHAR(10) PRIMARY KEY, 
 	bal NUMERIC(8, 0)
 );
 
-INSERT INTO Account VALUES ('AAAA10001', 98730);
-INSERT INTO Account VALUES ('AAAA10002', 48810);
-INSERT INTO Account VALUES ('AAAA10003', 61730);
-INSERT INTO Account VALUES ('AAAA10004', 10170);
-INSERT INTO Account VALUES ('AAAA10005', 69480);
-INSERT INTO Account VALUES ('AAAA10006', 34050);
-INSERT INTO Account VALUES ('AAAA10007', 47030);
+INSERT INTO ZAccount VALUES ('1001', 98730);
+INSERT INTO ZAccount VALUES ('1002', 48810);
+INSERT INTO ZAccount VALUES ('1003', 61730);
+INSERT INTO ZAccount VALUES ('1004', 10170);
+INSERT INTO ZAccount VALUES ('1005', 69480);
+INSERT INTO ZAccount VALUES ('1006', 34050);
+INSERT INTO ZAccount VALUES ('1007', 47030);
 
-DROP TABLE Transaction;
+DROP TABLE ZTransaction;
 
-CREATE TABLE Transaction (
+CREATE TABLE ZTransaction (
 	ano VARCHAR(10),
 	amt NUMERIC(8, 0)
 );
 
-CREATE OR REPLACE TRIGGER Banktrans
-BEFORE INSERT ON Transaction FOR EACH ROW
+CREATE OR REPLACE TRIGGER ZBanktrans
+BEFORE INSERT ON ZTransaction FOR EACH ROW
 DECLARE
-	kcount NUMBER(1) := 0;
+	kcount NUMERIC(8, 0) := 0;
 	kbal FLOAT := 0;
 BEGIN
-	SELECT bal INTO kbal FROM Account WHERE ano = :NEW.ano;
-	IF :NEW.ano < 0 THEN
-		RAISE_APPLICATION_ERROR(-1, 'Wrong account!');
-	ELSIF kbal - :NEW.amt < 0 THEN
-		RAISE_APPLICATION_ERROR(-2, 'Low balance!');
-	ELSIF :NEW.amt < 1 THEN
-		RAISE_APPLICATION_ERROR(-3, 'Cannot withdraw negative amount!');
+	SELECT count(*) INTO kcount FROM ZAccount WHERE ano = :NEW.ano;
+	IF kcount < 1 THEN
+		DBMS_OUTPUT.PUT_LINE('Wrong Account!');
+		RAISE_APPLICATION_ERROR(-1, 'Wrong Account!');
 	ELSE
-		UPDATE Account SET bal = bal - :NEW.amt WHERE ano = :NEW.ano;
+		SELECT bal INTO kbal FROM ZAccount WHERE ano = :NEW.ano;
+		IF kbal - :NEW.amt < 0 THEN
+			DBMS_OUTPUT.PUT_LINE('Low balance!');
+			RAISE_APPLICATION_ERROR(-2, 'Low balance!');
+		ELSIF :NEW.amt < 1 THEN
+			DBMS_OUTPUT.PUT_LINE('Cannot withdraw negative amount!');
+			RAISE_APPLICATION_ERROR(-3, 'Cannot withdraw negative amount!');
+		ELSE
+			UPDATE ZAccount SET bal = bal - :NEW.amt WHERE ano = :NEW.ano;
+			DBMS_OUTPUT.PUT_LINE('Updated!');
+		END IF;
 	END IF;
 END;
 /
+
+INSERT INTO ZTransaction VALUES ('1006', 1000);
+INSERT INTO ZTransaction VALUES ('1007', 1000);
