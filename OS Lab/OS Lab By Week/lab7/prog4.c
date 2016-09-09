8777abd493df90a8b1e462c83870cabccd5804c7
ykl7 <yklal95@gmail.com>
2016-09-09 23:37:16 +0530

    fix inter process comm using shared memory

--- OS Lab/OS Lab By Week/lab7/prog4.c
+++ OS Lab/OS Lab By Week/lab7/prog4.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main()
 {
@@ -10,8 +12,19 @@ int main()
     segment_id = shmget(IPC_PRIVATE, size, S_IRUSR | S_IWUSR);
     shared_memory = (char *) shmat(segment_id, NULL, 0);
     sprintf(shared_memory, "Inter process communication demo");
-    printf("%s\n", shared_memory);
-    shmdt(shared_memory);
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        printf("In child\n");
+        puts(shared_memory);
+        shmdt(shared_memory);
+    }
+    else
+    {
+        printf("In parent\n");
+        puts(shared_memory);
+        shmdt(shared_memory);
+    }
     shmctl(segment_id, IPC_RMID, NULL);
     return 0;
 }
